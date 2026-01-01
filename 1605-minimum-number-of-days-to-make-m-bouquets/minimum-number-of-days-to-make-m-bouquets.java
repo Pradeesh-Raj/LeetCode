9341d32960af55bc9b5942a1aa1b6cf2c9f16edc
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-01-01 14:49:08 +0530

    Time: 23 ms (18.86%) | Memory: 82.6 MB (41.69%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,49 @@
+class Solution {
+    public static int maxElement(int[] nums, int N){
+        int max = nums[0];
+        for(int i = 1 ; i < N ; i++){
+            max = (nums[i] > max) ? nums[i] : max;
+        }
+        return max;
+    }
+    public static int minElement(int[] nums, int N){
+        int min = nums[0];
+        for(int i = 1 ; i < N ; i++){
+            min = (nums[i] < min) ? nums[i] : min;
+        }
+        return min;
+    }
+
+    public static boolean possibleBouquets(int[] arr, int m, int k, long day, int N){
+        int count = 0, bouquets = 0;
+        for(int i = 0 ; i < N ; i++){
+            if(arr[i] <= day){
+                count++;
+            }
+            else{
+                bouquets += (count / k);
+                count = 0;
+            }
+        }
+        bouquets += (count / k);
+        if(bouquets >= m) return true;
+        return false;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        int N = bloomDay.length;
+        int low = minElement(bloomDay, N);
+        int high = maxElement(bloomDay, N);
+        int res = Integer.MAX_VALUE;
+        while(low <= high){
+            long mid = low + (high-low)/2;
+            if(possibleBouquets(bloomDay, m, k, mid, N)){
+                res = Math.min(res, (int)mid);
+                high = (int)mid - 1;
+            }
+            else low = (int)mid + 1;
+        }
+        if(res == Integer.MAX_VALUE) return -1;
+        return res;
+    }
+}
