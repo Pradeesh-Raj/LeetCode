7217e23a184d2366a1cb378f8448538dc80c5ef0
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-01-01 10:08:39 +0530

    Time: 20 ms (42.60%) | Memory: 47.8 MB (70.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -8,7 +8,7 @@ class Solution {
     }
     public static int maxElement(int[] arr){
         int max = arr[0];
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i = 1 ; i < arr.length ; i++){
             if(arr[i] > max) max = arr[i];
         }
         return max;
