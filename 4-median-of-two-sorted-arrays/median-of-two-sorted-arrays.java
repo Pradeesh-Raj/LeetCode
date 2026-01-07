2186e64c390367f9f0b5f842c308bfaa5d9a1463
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-01-07 20:52:00 +0530

    Time: 1 ms (99.64%) | Memory: 48.8 MB (55.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,44 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int N = n + m;
-        int idx2 = N / 2;
-        int idx1 = idx2 - 1;
-        int ele1 = -1,ele2 = -1;
-        int count = 0;
-        int i = 0 , j = 0;
-        while(i < n && j < m){
-            if(nums1[i] < nums2[j]){
-                if(count == idx1) ele1 = nums1[i];
-                if(count == idx2) ele2 = nums1[i];
-                count++;
-                i++;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int N = n1+n2;
+        if(n1 > n2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int left = (n1+n2+1) / 2;
+        int low = 0, high = n1;
+        while(low <= high){
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid1 < n1){
+                r1 = nums1[mid1];
             }
-            else{
-                if(count == idx1) ele1 = nums2[j];
-                if(count == idx2) ele2 = nums2[j];
-                count++;
-                j++;
+            if(mid2 < n2){
+                r2 = nums2[mid2];
             }
+            if(mid1 - 1 >= 0){
+                l1 = nums1[mid1-1];
+            }
+            if(mid2 - 1 >= 0){
+                l2 = nums2[mid2-1];
+            }
+            if(l1 <= r2 && l2 <= r1){
+                if((N & 1) != 0){
+                    return Math.max(l1,l2);
+                }
+                else{
+                    return (double)(Math.max(l1,l2) + Math.min(r1,r2)) / 2d;
+                }
+            }
+            else if(l1 > r2){
+                high = mid1-1;
+            }
+            else low = mid1+1;
         }
-        while(i < n){
-            if(count == idx1) ele1 = nums1[i];
-            if(count == idx2) ele2 = nums1[i];
-            count++;
-            i++;
-        }
-        while(j < m){
-            if(count == idx1) ele1 = nums2[j];
-            if(count == idx2) ele2 = nums2[j];
-            count++;
-            j++;
-        }
-        if((N & 1) != 0){
-            return (double)ele2;
-        }
-        else{
-            return (double)(ele1+ele2) / 2;
-        }
+        return -1;
     }
 }
