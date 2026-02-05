d24e5729df3c7a7247696142910e1a0fe28d8ecb
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-02-05 16:43:43 +0530

    LC-transformed-array [Runtime: N/A; Memory: N/A]

---
+++ array/transformed-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int N = nums.length;
+        int[] result = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            if(nums[i] > 0){
+                result[i] = nums[(i + nums[i]) % N];
+            }
+            else if(nums[i] < 0){
+                result[i] = nums[(i - (-nums[i]) + N) % N];
+            }
+            else result[i] = nums[i];
+        }
+        return result;
+    }
+}
