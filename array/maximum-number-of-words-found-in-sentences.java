4b5cfe6f36b2881f7e1a5798b01c3e330ecbac3e
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-02-07 18:29:10 +0530

    LC-maximum-number-of-words-found-in-sentences [Runtime: 3 ms; Memory: 46 MB]

--- array/maximum-number-of-words-found-in-sentences.java
+++ array/maximum-number-of-words-found-in-sentences.java
@@ -1,8 +1,8 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
         int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < sentences.length ; i++){
-            max = Math.max(max,sentences[i].split("\\s+").length);
+        for(String sentence : sentences){
+            max = Math.max(max,sentence.split(" ").length);
         }
         return max;
     }
