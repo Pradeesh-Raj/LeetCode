57832645d91fa7f08cd978647fe290179f8c0541
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-02-11 22:51:48 +0530

    LC-matrix-diagonal-sum [Runtime: 0 ms; Memory: 46.6 MB]

---
+++ matrix/matrix-diagonal-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for(int i = 0 ; i < n ; i++){
+            if(i != n-i-1){
+                sum += mat[i][i] + mat[i][n-i-1];
+            }
+            else sum += mat[i][i];
+        }
+        return sum;
+    }
+}
