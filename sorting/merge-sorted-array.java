30d2187185760ad4abe4721286d810d3ac7495da
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-02-26 23:51:16 +0530

    LC-merge-sorted-array [Runtime: 4 ms; Memory: 44.2 MB]

--- sorting/merge-sorted-array.java
+++ sorting/merge-sorted-array.java
@@ -1,47 +1,16 @@
 class Solution {
-    public static void swap(int[] n1, int[] n2, int index1,int index2){
-        int temp = n1[index1];
-        n1[index1] = n2[index2];
-        n2[index2] = temp;
-    }
-
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(m == 0) {
-            for(int i = 0 ; i < n ; i++){
-                nums1[i-m] = nums2[i];
-            }
-            return;
+        int i = m-1, j = 0;
+        while(i >= 0 && j < n && nums1[i] > nums2[j]){
+            int temp = nums1[i];
+            nums1[i] = nums2[j];
+            nums2[j] = temp;
+            i--;j++;
         }
-        if(n == 0){
-            return;
-        }
-        int len = m + n;
-        int gap = (len / 2) + (len % 2);
-        while(gap > 0){
-            int left = 0, right = left + gap;
-            while(right < len){
-                if(left < m && right >= m){
-                    if(nums1[left] > nums2[right - m]){
-                        swap(nums1,nums2,left,right-m);
-                    }
-                }
-                else if(left >= m){
-                    if(nums2[left - m] > nums2[right - m]){
-                        swap(nums2,nums2,left-m,right-m);
-                    }
-                }
-                else{
-                    if(nums1[left] > nums1[right]){
-                        swap(nums1,nums1,left,right);
-                    }
-                }
-                left++;right++;
-            }
-            if(gap == 1) break;
-            gap = (gap / 2) + (gap % 2);
-        }
-        for(int i = m ; i < len ; i++){
-            nums1[i] = nums2[i-m];
+        Arrays.sort(nums1,0,m);
+        Arrays.sort(nums2);
+        for(int k = m ; k < m+n ; k++){
+            nums1[k] = nums2[k-m];
         }
     }
 }
