5a5886fc78d444d2006a730add5810641ff8cf51
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-03-07 11:01:41 +0530

    LC-longer-contiguous-segments-of-ones-than-zeros [Runtime: 1 ms; Memory: 42.7 MB]

---
+++ string/longer-contiguous-segments-of-ones-than-zeros.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int count1s = 0, count0s = 0;
+        int maxCount1 = 0, maxCount0 = 0;
+        for(int i = 0 ; i < s.length() ; i++) {
+            if(s.charAt(i) == '1') {
+                count0s = 0;
+                count1s++;
+                maxCount1 = Math.max(maxCount1, count1s);
+            }
+            else {
+                count1s = 0;
+                count0s++;
+                maxCount0 = Math.max(maxCount0, count0s);
+            }
+        }
+        if(maxCount0 == maxCount1) return false;
+        return ((maxCount0 > maxCount1) ? false : true);
+    }
+}
