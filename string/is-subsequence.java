b45f7ff815449afbe0423a3c78a6c956906ae76f
Pradeesh Raj S <pradeeshs835@gmail.com>
2026-03-25 23:52:21 +0530

    LC-is-subsequence [Runtime: 2 ms; Memory: 42.9 MB]

--- string/is-subsequence.java
+++ string/is-subsequence.java
@@ -1,10 +1,15 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Set<Character> S = new LinkedHashSet<>();
-        Set<Character> T = new LinkedHashSet<>();
-        for(char i : s.toCharArray()) S.add(i);
-        for(char i : t.toCharArray()) T.add(i);
-        T.retainAll(S);
-        return T.size() == S.size();
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;j++;
+            }
+            else {
+                j++;
+            }
+        }
+        if(i > s.length()-1) return true;
+        return false;
     }
 }
